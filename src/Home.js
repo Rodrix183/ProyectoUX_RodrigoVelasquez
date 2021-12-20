cba28ce54f8473481c5b961c6c0db85b85305095
Rodrigo Velásquez <38990525+Rodrix183@users.noreply.github.com>
2021-12-19 20:39:00 -0600

    Modificando la validacion
    Registrar con exito, cambiar fondo

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,11 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import  Lista  from './lista';
+import Lista from './lista';
 import { NavigationBar } from './components/NavigationBar';
-import { useFirestoreDocData, useFirestore, useFirestoreCollectionData, useAuth, useUser, auth} from 'reactfire';
+import { useFirestoreDocData, useFirestore, useFirestoreCollectionData, useAuth, useUser, auth } from 'reactfire';
 import { BrowserRouter as Router, Route, Switch, Link, useHistory } from 'react-router-dom';
-import {Card} from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import styled from 'styled-components';
 
 const Styles = styled.div`
@@ -18,15 +18,16 @@ const Styles = styled.div`
     justify-content: flex-start;
     font-size: calc(10px + 2vmin);
     color: white;
+    background-color: rgb(154, 204, 204);
 `;
 
 //export const Home = () => (
-export function Home(){
+export function Home() {
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
 
     const auth = useAuth();
-      
+
 
     function registrar() {
         auth.createUserWithEmailAndPassword(email, password).catch(err => console.log(err));
@@ -43,48 +44,58 @@ export function Home(){
         auth.signInWithEmailAndPassword(email, password).then(() => handleClick("/lista")).catch(err => console.log(err));
     };
 
-    function notificacionRegistro(){
-        console.log('BUENAS');
-        
+    function notificacion() {
+        alert("Registrado con éxito!");
     }
 
-    return(
-    <Styles>
-        <div>
-            <NavigationBar/>
-            <h1>
-                x
+    function funciones() {
+        registrar();
+        notificacion();
+        limpiar();
+    }
 
-            </h1>
-            <h1>
-                x
-            </h1>
-            <Card style={{ width: '50rem' , height : '30rem'}}>
-            <Form>
-                
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label></Form.Label>
-                    <Form.Control type="email" placeholder="Ingrese su correo electrónico"  value={email} onChange={e => setEmail(e.target.value)}/>
-                </Form.Group>
-                <br></br>
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Ingrese su contraseña" value={password} onChange={e => setPassword(e.target.value)}/>
-                </Form.Group>
-                <br></br>   
-                    <Button style={{width : '40rem'}} onClick={ingresar} >
-                        Ingresar
-                    </Button>
+    function limpiar(){
+        setEmail("");
+        setPassword("");
+    }
+
+    return (
+        <Styles>
+            <div>
+                <NavigationBar />
                 <h1>
+                    x
+
                 </h1>
-                <br></br>
-                <Button style={{width : '40rem'}} onClick={registrar , notificacionRegistro}>
-                    Registrar
-                </Button>
-                <h1></h1>
-            </Form>
-            </Card>
-        </div>
-    </Styles>
-    );    
+                <h1>
+                    x
+                </h1>
+                <Card style={{ width: '50rem', height: '30rem' }}>
+                    <Form>
+
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label></Form.Label>
+                            <Form.Control type="email" placeholder="Ingrese su correo electrónico" value={email} onChange={e => setEmail(e.target.value)} />
+                        </Form.Group>
+                        <br></br>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Control type="password" placeholder="Ingrese su contraseña" value={password} onChange={e => setPassword(e.target.value)} />
+                        </Form.Group>
+                        <br></br>
+                        <Button style={{ width: '40rem' }} onClick={ingresar} >
+                            Ingresar
+                        </Button>
+                        <h1>
+                        </h1>
+                        <br></br>
+                        <Button style={{ width: '40rem' }} onClick={funciones}>
+                            Registrar
+                        </Button>
+                        <h1></h1>
+                    </Form>
+                </Card>
+            </div>
+        </Styles>
+    );
 }
 //)
